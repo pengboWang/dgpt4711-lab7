9862fedfcdf8b9808d108e10259832d263a6a92b
pengboWang <1006736449@qq.com>
2020-11-04 14:26:46 +0800

    Commit JSON

---
+++ app/Models/Places.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Models;
+use App\Models\Simple\JSONModel;
+/*
+ * Mock travel destination data.
+ * Note that we don't have to extend CodeIgniter's model for now
+ */
+
+class Places extends JSONModel{
+
+    //mock data : an array of records
+protected $origin = WRITEPATH . 'data/placesData.json';
+protected $keyField = 'id';
+protected $validationRules = [];
+}
